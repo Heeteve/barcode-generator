634f537b89ed17f020c8d34d40825c6f5c42ad09
Heeteve <a13267110880@qq.com>
2026-09-13 02:02:34 +0800

    rm i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,42 +1,21 @@
 import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 
-// Can be imported from a shared config
-export const locales = [
-  'en',
-  'zh',
-  'ja',
-  'de',
-  'fr',
-  'es',
-  'it',
-  'nl',
-  'sv',
-  'ko',
-  'ru',
-]
+// 可从共享配置中导入
+export const locales = ['en', 'zh']
 
 export const languageNames: Record<Locale, string> = {
   en: 'English',
   zh: '中文',
-  ja: '日本語',
-  de: 'Deutsch',
-  fr: 'Français',
-  es: 'Español',
-  it: 'Italiano',
-  nl: 'Nederlands',
-  sv: 'Svenska',
-  ko: '한국어',
-  ru: 'Русский',
 }
 
 export type Locale = (typeof locales)[number]
 export const defaultLocale = 'en'
-// Use the default: `always`，设置为 as-needed可不显示默认路由
+// 使用默认值 always；设置为 as-needed 可隐藏默认语言路由
 export const localePrefix = 'always'
 
 export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
+  // 校验传入的语言参数是否有效
   if (!locales.includes(locale as any)) notFound()
 
   return {
